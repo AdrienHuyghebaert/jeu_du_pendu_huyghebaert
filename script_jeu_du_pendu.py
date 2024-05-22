474c73cb1561f5cb510a10d57359813bea349c2c
Adrien Huyghebaert <adrien.huyghebaert.1@ens.etsmtl.ca>
2024-05-22 18:24:22 -0400

    Changement de la fonction pour enlever les accents et divers correctifs

--- script_jeu_du_pendu.py
+++ script_jeu_du_pendu.py
@@ -1,79 +1,73 @@
+import unicodedata
+import os
+
+
 # Cette procédure permet de dire bonjour à l'utilisateur.
 def dire_bonjour():
-    print("    Bonjour et bienvenue sur le jeu du pendu !\n")
+    print("Bonjour et bienvenue sur le jeu du pendu !\n")
 
 
 # Cette fonction permet de demander à l'utilisateur si il souhaite
 # utliser sa liste de mots personnelle ou celle par défaut
 # et de retourner un mot au hasard.
 def choisir_liste():
-    print("""\
-    Le jeu possède une liste de mots par défaut.
-    Mais vous pouvez ajouter la vôtre sous forme d'un fichier texte nommé "mots_pendu" dans le dossier du jeu.""")
+    print("Le jeu possède une liste de mots par défaut, mais vous pouvez jouer la vôtre.")
+    choix_liste_mots = input("Si vous souhaitez utiliser votre liste de mots, rentrez oui, sinon rentrez non :\n")
 
-    choix_liste_mots = int(input("""
-    Si vous souhaitez utiliser votre liste de mots, tapez 1
-    et si vous souhaitez utiliser la liste par défaut, tapez 2: """))
-
+    # Cette boucle "while" permet de d'assurer tant que l'utilisateur n'a pas rentré autre chose que oui ou non
     mot = ""
-    while mot == "":  # Cette boucle "while" permet de d'assurer tant que l'utilisateur n'a pas rentré 1 ou 2
-        if choix_liste_mots == 1:
-            with open("test_liste.txt", 'r') as fio:
+    while mot == "":
+        if choix_liste_mots == 'non':
+            with open("mots_pendu_defaut.txt", 'r', encoding='utf-8') as fio:
                 mots = fio.read().splitlines()
             from random import choice
             mot = choice(mots)
             return mot
 
-        elif choix_liste_mots == 2:
-            with open("mots_pendu_defaut.txt", 'r') as fio:
+        elif choix_liste_mots == 'oui':
+            nom_fichier = input("Rentrez le nom de votre fichier\n")
+            chemin_fichier = input("Rentrez le chemin pour accèder à votre fichier\n")
+            fichier_utilisateur = os.path.join(chemin_fichier, nom_fichier + '.txt')
+            with open(fichier_utilisateur, 'r', encoding='utf8') as fio:
                 mots = fio.read().splitlines()
             from random import choice
             mot = choice(mots)
             return mot
 
         else:
-            print("\n\n\n\n\n"
-                  "    Choix non valide. Veuillez taper 1 ou 2.")
-
-            choix_liste_mots = int(input("\n"
-                                         "    Si vous souhaitez utiliser votre liste de mots, tapez 1\n"
-                                         "    et si vous souhaitez utiliser la liste par défaut, tapez 2: "))
+            choix_liste_mots = input("Choix non valide. Veuillez rentrer oui ou non :\n")
 
 
 # Cette fonction permet de demander à l'utilisateur
 # combien il souhaite d'essais pour trouver le mot
 def choisir_nombre_essais():
-    print("\n"    
-          "    Par défaut, le jeu propose de trouver le mot en 6 tentatives.")
-    changement_nb_essais = int(input("\n"
-                                     "    Si vous souhaitez changer le nombre de tentatives, tapez 2. Sinon tapez 1 "))
+    print("Par défaut, le jeu propose de trouver le mot en 6 tentatives.")
+    changement_nb_essais = int(input("Si vous souhaitez changer le nombre de tentatives, tapez 1, sinon tapez 2 :\n"))
 
-    if changement_nb_essais == 1:
+    if changement_nb_essais == 2:
         nombre_essais = 6
         return nombre_essais
 
-    elif changement_nb_essais == 2:
-        nombre_essais = int(input("    Combien d'essais souhaitez-vous ? (Rentrez un nombre entier) "))
+    elif changement_nb_essais == 1:
+        nombre_essais = int(input("Combien d'essais souhaitez-vous ? (Rentrez un nombre entier)\n"))
         return nombre_essais
 
 
-# Cette fonction prend en argument le mot choisi et permet de retirer les accents
+# Cette fonction permet de supprimer les éventuels accents du mot
 def supprimer_accents(mot_choisi):
-    liste_mot_choisi = list(mot_choisi)  # Cette liste permet de convertir ma chaine de caractère en liste
-    liste_accents = ['à', 'â', 'ä', 'é', 'è', 'ê', 'ë', 'î', 'ï', 'ô', 'ö', 'ù', 'û', 'ü', 'ÿ', 'ç']
-    liste_sans_accents = ['a', 'a', 'a', 'e', 'e', 'e', 'e', 'i', 'i', 'o', 'o', 'u', 'u', 'u', 'y', 'c']
-    for i in range(len(liste_mot_choisi)):
-        for j in range(len(liste_accents)):
-            if liste_mot_choisi[i] == liste_accents[j]:
-                liste_mot_choisi[i] = liste_sans_accents[j]
-    return liste_mot_choisi
+    mot_sans_accents = unicodedata.normalize('NFKD', mot_choisi).encode('ASCII', 'ignore').decode('utf-8')
+    return mot_sans_accents
 
 
-# Cette section est la partie principale du code
-dire_bonjour()
-mot_choisi = choisir_liste()
-print(mot_choisi)
-nombre_essais = choisir_nombre_essais()
-print(nombre_essais)
-liste_mot_choisi = supprimer_accents(mot_choisi)
-print(liste_mot_choisi)
+# Ceci est la fonction principale qui appelle les autres fonctions et procédures
+def jeu_du_pendu():
+    dire_bonjour()
+    mot_choisi = choisir_liste()
+    print(mot_choisi)
+    nombre_essais = choisir_nombre_essais()
+    print(nombre_essais)
+    mot_sans_accents = supprimer_accents(mot_choisi)
+    print(mot_sans_accents)
+
+
+jeu_du_pendu()
